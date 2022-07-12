38530771c12bb921e278cafbdf327b7fffeb165f
Bryce Wein <bryce.wein@gmail.com>
2022-07-12 10:25:24 -0400

    Feat/Test: fetch single item

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: %i[index show]
-      resources :items, only: %i[index]
+      resources :items, only: %i[index show]
     end
   end
 end
