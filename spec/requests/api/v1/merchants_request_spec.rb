de998511c48265718a0cd24893e0b9407cef4bc4
Bryce Wein <bryce.wein@gmail.com>
2022-07-12 10:20:06 -0400

    Feat/Test: fetch single merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'Merchants API' do
 
       merchants.each do |merchant|
         expect(merchant).to have_key :id
-        expect(merchant[:id]).to be_a Integer
+        expect(merchant[:id]).to be_a String
 
         expect(merchant).to have_key :type
         expect(merchant[:type]).to eq('merchant')
@@ -31,6 +31,35 @@ RSpec.describe 'Merchants API' do
         expect(merchant).to_not have_key :created_at
         expect(merchant).to_not have_key :updated_at
       end
+    end
+
+    it 'sends a singular merchant' do
+      merchant_id = create(:merchant).id
+
+      get "/api/v1/merchants/#{merchant_id}"
+
+      expect(response).to be_successful
+
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_body).to have_key(:data)
+      expect(parsed_body[:data]).to be_a Hash
+
+      merchant = parsed_body[:data]
+
+      expect(merchant).to have_key :id
+      expect(merchant[:id]).to be_a String
+
+      expect(merchant).to have_key :type
+      expect(merchant[:type]).to eq('merchant')
+
+      expect(merchant).to have_key :attributes
+
+      expect(merchant[:attributes]).to have_key :name
+      expect(merchant[:attributes][:name]).to be_a String
+
+      expect(merchant).to_not have_key :created_at
+      expect(merchant).to_not have_key :updated_at
     end
   end
 end
